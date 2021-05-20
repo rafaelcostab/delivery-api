b46596c85acc55de56fd567663148104524b3c49
Rafael Costa <rafael.costab@hotmail.com>
2021-05-19 23:47:06 -0300

    2.5 validating with bean validation

--- src/main/java/com/rafaelcostab/delivery/api/controller/ClientController.java
+++ src/main/java/com/rafaelcostab/delivery/api/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.rafaelcostab.delivery.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,12 +42,12 @@ public class ClientController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Client add(@RequestBody Client client) {
+	public Client add(@Valid @RequestBody Client client) {
 		return clientRepository.save(client);
 	}
 	
 	@PutMapping("/{clientId}")
-	public ResponseEntity<Client> update (@PathVariable Long clientId, @RequestBody Client client){
+	public ResponseEntity<Client> update (@PathVariable Long clientId, @Valid @RequestBody Client client){
 		if (!clientRepository.existsById(clientId)) {
 			return ResponseEntity.notFound().build();
 		}
@@ -65,7 +67,6 @@ public class ClientController {
 		clientRepository.deleteById(clientId);
 		
 		return ResponseEntity.noContent().build();
-
 	}
 	
 	
