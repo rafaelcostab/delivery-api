74f5ed0adf107ea537ed602f71cf97b516908eac
Rafael Costa <rafael.costab@hotmail.com>
2021-05-19 23:27:28 -0300

    2.4 implementing the crud of client

--- src/main/java/com/rafaelcostab/delivery/api/controller/ClientController.java
+++ src/main/java/com/rafaelcostab/delivery/api/controller/ClientController.java
@@ -2,7 +2,16 @@ package com.rafaelcostab.delivery.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelcostab.delivery.domain.model.Client;
@@ -12,13 +21,52 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
 	private ClientRepository clientRepository;
 	
-	@GetMapping("/clients")
+	@GetMapping
 	public List<Client>find() {
 		return clientRepository.findAll();
 	}
+	
+	@GetMapping("/{clientId}")
+	public ResponseEntity<Client> findById(@PathVariable Long clientId) {
+		return clientRepository.findById(clientId)
+			.map(ResponseEntity::ok)
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client add(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Client> update (@PathVariable Long clientId, @RequestBody Client client){
+		if (!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(clientId);
+		client = clientRepository.save(client);
+		
+		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{clientId}")
+	public ResponseEntity<Void> delete(@PathVariable Long clientId){
+		if (!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(clientId);
+		
+		return ResponseEntity.noContent().build();
+
+	}
+	
 	
 }
